e8f8c24a741551cfcfbaaca99d51ac97a9d29eb1
João Victor Bigardi <128434230+bigardii@users.noreply.github.com>
2024-04-19 21:54:33 -0300

    Update processar_formulario.php

--- processar_formulario.php
+++ processar_formulario.php
@@ -1,14 +1,16 @@
 <?php
 
     // Obtém os dados do formulário
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $mensagem = $_POST["mensagem"];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $mensagem = $_POST['mensagem'];
 
     // Configurações de e-mail
     $destinatario = "joaovbp1234@hotmail.com";
     $assunto = "Formulário de Contato";
-    $corpo = "Nome: " . $nome . "\n" ."Email: " . $email . "\n" . "Mensagem: " . $mensagem;
+    $corpo = "Nome: " . $nome . "\n" ."Email: " . $email ."\n" . "Telefone: ".$telefone . "\n" . "Mensagem: " . $mensagem;
+
 
     // Envia o e-mail
     $cabeca = "From: joaovbp1234@hotmail.com". "\n". "Reply-to: ".$email."\n". "X=Mailer:PHP/".phpversion();
